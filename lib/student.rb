a29f23c7218b6eff99c94e131c06010495c342dc
conner fortner <fortner.conner@yahoo.com>
2020-11-09 21:23:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,51 @@ class Student
       grade TEXT
     )
     SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+       self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
+  def self.create(name, grade)
+    students = Students.new(name, grade)
+    students.save
+    students
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(name, grade, id)
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map {|row| new_from_db(row)}.first
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end  
+    
+        
 
 
 end
